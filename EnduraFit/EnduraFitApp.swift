8f1532ee7a6d2d974d25448f4d2be5bc71113555
Joao Caetano <joao.caetano@revolut.com>
2024-12-08 18:40:12 +0000

    Initial Commit

---
+++ EnduraFit/EnduraFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnduraFitApp.swift
+//  EnduraFit
+//
+//  Created by João Valdeira Caetano on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnduraFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnduraFitTests/EnduraFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnduraFitTests.swift
+//  EnduraFitTests
+//
+//  Created by João Valdeira Caetano on 08/12/2024.
+//
+
+import Testing
+@testable import EnduraFit
+
+struct EnduraFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
